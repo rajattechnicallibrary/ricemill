4a2b1d0dfaed0f5780744d04037491dba9da0645
rajatguptaTechlib <rajatinvoice@gmail.com>
2021-06-06 22:11:25 +0530

    no message

--- application/views/elements/footer.php
+++ application/views/elements/footer.php
@@ -1,5 +1,7 @@
 <!-- BEGIN FOOTER -->
 <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
-    <span>Copyright © <?= date('Y') ?>  All rights reserved.</span>
+    <span>Copyright © <?= date('Y') ?>  All Rights Reserved.</span>
+    <span>This Website is Designed, Developed By: -  <b style="color:blue"> C R Industries ( Er. Rajat Gupta ) </b> </span>
+    
 </footer>
 <!-- END FOOTER -->
